539d091a81abf48da7992125a1c8088a128e99f6
oliverslott <oliverslottsony@gmail.com>
2019-10-29 21:16:31 +0100

    Changed to mongoose because they makes this way easier..

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require("cors");
 var bodyParser = require('body-parser');
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 var app = express();
 let port = 3001;
 if (process.env.NODE_ENV === 'prod') {
@@ -9,82 +11,67 @@ if (process.env.NODE_ENV === 'prod') {
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
-
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
 const uri = "mongodb+srv://Oliver:RME4gUT1U8xaOpMH@cluster0-juyfc.azure.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-let db;
 
-client.connect(err => {
-    if(err) console.error(err);
-    db = client.db("WorldCruise");
-});
+mongoose.connect(uri, {dbName: "WorldCruise"});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 //Bruger Start
 
+let BrugerSchema = new Schema({
+    Navn: String,
+    Adresse: String,
+    Email: String,
+    Kodeord: String,
+    Nationalitet: String,
+    Penge: String,
+    Aktiv: Boolean
+});
+let BrugerModel = mongoose.model("Bruger", BrugerSchema, "Bruger");
+
 app.get("/bruger", function(req, res){    
-    const collection = db.collection("Bruger");
-    let cursor = collection.find({});
-    cursor.toArray().then(content => {
-        res.send(content);
+    BrugerModel.find({}, function(err, bruger){
+        res.send(bruger);
     });
 });
 
 app.post("/bruger/opret", function(req, res){
-    const collection = db.collection("Bruger");
-    collection.insertOne({
-        Navn: req.body.Navn,
-        Adresse: req.body.Adresse,
-        Email: req.body.Email,
-        Kodeord: req.body.Kodeord,
-        Nationalitet: req.body.Nationalitet,
-        Penge: 0,
-        Aktiv: true
-    }, function(){
-        res.send(); //200 ok?
-    });
+    BrugerModel.create(req.body, function(){
+        res.send();
+    })
 });
 
 app.post("/bruger/slet/:id", function(req, res){
-    const collection = db.collection("Bruger");
-    collection.deleteOne({
-        _id: new mongodb.ObjectID(req.params.id)
+    BrugerModel.deleteOne({_id: req.params.id}, function(err){
+        res.send();
     });
-    res.send(); //Ok
 });
 
 app.get("/bruger/hent/:id", function(req, res){
-    const collection = db.collection("Bruger");
-    let cursor = collection.find({
-        _id: new mongodb.ObjectID(req.params.id)
-    });
-    cursor.toArray().then(content => {
-        res.send(content); //Ok
+    BrugerModel.find({_id: req.params.id}, function(err, bruger){
+        res.send(bruger);
     });
 });
 
 //Bruger End
 
 //Administrator Start
+let AdminSchema = new Schema({
+    Navn: String,
+    Kodeord: String
+});
+let AdminModel = mongoose.model("Administrator", AdminSchema, "Administrator");
 
 app.get("/administrator", function(req, res){
-    const collection = db.collection("Administrator");
-    let cursor = collection.find({});
-    cursor.toArray().then(content => {
-        res.send(content);
+    AdminModel.find({}, function(err, admin){
+        res.send(admin);
     });
 });
 
 app.post("/administrator/opret", function(req, res) {
-    const collection = db.collection("Administrator");
-    collection.insertOne({
-        Navn: req.body.Navn,
-        Kodeord: req.body.Kodeord
-    }, function(){
-        res.send(); //Ok
+    AdminModel.create(req.body, function(){
+        res.send();
     });
 });
 
@@ -92,62 +79,49 @@ app.post("/administrator/opret", function(req, res) {
 
 //Butik Start
 
+let ButikSchema = new Schema({
+    Logo: String,
+    Navn: String,
+    Adresse: String,
+    Telefon: String,
+    Kodeord: String,
+    Details: String,
+    KontaktOplysninger: String
+});
+let ButikModel = mongoose.model("Butik", ButikSchema, "Butik");
+
+
 app.get("/butik", function(req, res){
-    const collection = db.collection("Butik");
-    let cursor = collection.find({});
-    cursor.toArray().then(content => {
-        res.send(content);
+    ButikModel.find({}, function(err, butik){
+        res.send(butik);
     });
 });
 
 app.get("/butik/:id", function(req, res){
-    const collection = db.collection("Butik");
-    let cursor = collection.find({_id: new mongodb.ObjectID(req.params.id)});
-    cursor.toArray().then(content => {
-        res.send(content);
+    ButikModel.findById(req.params.id, function(err, butik){
+        res.send(butik);
     });
 });
 
 
 //does delete, but does not redirect back
 app.post("/butik/slet/:id", function(req, res){
-    const collection = db.collection("Butik");
-    collection.deleteOne({
-        _id: new mongodb.ObjectID(req.params.id)
-    }, function(){
+    ButikModel.deleteOne({_id: req.params.id}, function(err){
         res.send(); //Ok
     });
 });
 
 app.post("/butik/opret", function(req, res){
-    const collection = db.collection("Butik");
-    collection.insertOne({
-        Logo: req.body.Logo,
-        Navn: req.body.Navn,
-        Adresse: req.body.Adresse,
-        Telefon: req.body.Telefon,
-        Kodeord: req.body.Kodeord,
-        Details: req.body.Details,
-        KontaktOplysninger: req.body.KontaktOplysninger
-    }, function(){
+    ButikModel.create(req.body,function(){
         res.send();
     });
 });
 
 app.post("/butik/edit/:id", function(req, res){
-    const collection = db.collection("Butik");
-    collection.update({
-        _id: new mongodb.ObjectID(req.params.id)
-    },{
-        Logo: req.body.Logo,
-        Navn: req.body.Navn,
-        Adresse: req.body.Adresse,
-        Telefon: req.body.Telefon,
-        Kodeord: req.body.Kodeord,
-        Details: req.body.Details,
-        KontaktOplysninger: req.body.KontaktOplysninger
-    }, function(){
-        res.send();
+    ButikModel.findOneAndUpdate({
+        _id: req.params.id
+    }, req.body, {}, function(err, doc){
+        res.send(); //Ok
     });
 });
 
