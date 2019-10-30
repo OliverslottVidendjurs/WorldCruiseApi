59f4dae5c7d6ec47bf79e80a7ec2fffd3c76adf4
oliverslott <oliverslottsony@gmail.com>
2019-10-30 13:37:59 +0100

    fixed login bug

--- app.js
+++ app.js
@@ -63,8 +63,12 @@ app.post("/bruger/edit/:id", function(req, res){
 
 app.post("/bruger/login", function(req, res){
     BrugerModel.findOne({Email: req.body.Email}, function(err, bruger){
-        if(bruger.Kodeord == req.body.Kodeord){
-            res.send(bruger.id);
+        if(bruger) {
+            if(bruger.Kodeord == req.body.Kodeord){
+                res.send(bruger.id);
+            } else {            
+                res.status(400).send("Wrong email or password!");
+            }
         } else {            
             res.status(400).send("Wrong email or password!");
         }
